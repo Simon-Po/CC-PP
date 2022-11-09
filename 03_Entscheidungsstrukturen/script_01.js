3bef0a535e7e2e39bf7b2f632234ac70a2c4a700
Simon Pohl <simonnogam@gmail.com>
2022-11-09 12:42:21 +0100

    ++ entscheidungs strukturen

---
+++ 03_Entscheidungsstrukturen/script_01.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assigment
+const ageJohn = 35;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
