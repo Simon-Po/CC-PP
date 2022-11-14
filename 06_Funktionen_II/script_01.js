245d04d293f398f2262aae90252e97710bd2039a
Simon Pohl <simonnogam@gmail.com>
2022-11-14 09:08:28 +0100

    ++ addition

--- 06_Funktionen_II/script_01.js
+++ 06_Funktionen_II/script_01.js
@@ -8,3 +8,24 @@
 4. Ausgabe in Konsole :
 */
 
+
+
+
+
+
+
+
+
+
+// returns a+b as an int test:
+// out(add(35,34));
+// out(add(-3,2));
+// out(add(2,0));
+function add(a,b) {
+    return a + b;
+}
+
+// module: console output
+function out(outputData) {
+    console.log(outputData);
+}
