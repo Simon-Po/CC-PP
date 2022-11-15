106770f0f1e6f3dab1bcf425d71fcd418e8933c2
Simon Pohl <simonnogam@gmail.com>
2022-11-15 11:08:18 +0100

    added for loop and restructured func

--- 07_Arrays_I/script_01.js
+++ 07_Arrays_I/script_01.js
@@ -4,12 +4,26 @@
 
  
 /*** 01. Funktionalität mit Einzelparametern */
-// --> "Ich bin Max Mütze."
+
+
+// 02. Theorie Array
+
+
+// let arr;
+// //arr = new Array(); // erzeugung eines Arrays durch einen Constructor
+// arr = [];
+// arr = [2,7,11,4];
+// arr = [false,true,false];
+// arr = ["ich","bin","max","mütze"]
+
+// out(arr[arr.length-1])
+// out(arr.length)
+
+// --> "Ich bin Max Mütze." test:
 //out(getSentence("ich","bin","max","mütze")); 
-out(getSentence("ich","bin","der","coole","max","mütze"));
+//out(getSentence("ich","bin","der","coole","max","mütze"));
 function getSentence(word1,word2,word3,word4,word5,word6) {
-    const GAP   = " ";
-    const PUNKT = ".";
+    
     let str     =  word1 + GAP + 
                    word2 + GAP +
                    word3 + GAP +
@@ -17,12 +31,22 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
                    word5 + GAP + 
                    word6 + 
                    PUNKT;
-
-
-
-
-
     return str;
+}
+const GAP   = " ";
+const PUNKT = ".";
+
+
+
+out(getSentenceArr(["ich","bin","der","coole","max","mütze"]));
+
+function getSentenceArr(arr) {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        str = str +  arr[i] + GAP;
+        
+    }
+    return str+ PUNKT
 }
 
 
