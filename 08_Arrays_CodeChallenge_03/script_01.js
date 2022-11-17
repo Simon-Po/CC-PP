846b7cf54afa10f43603b405b6c78f7b5183ec15
Simon Pohl <simonnogam@gmail.com>
2022-11-17 08:15:06 +0100

    solved

--- 08_Arrays_CodeChallenge_03/script_01.js
+++ 08_Arrays_CodeChallenge_03/script_01.js
@@ -26,7 +26,6 @@ function output(p) {
 function getSentence(arr,letter) {
     let out = "";
     
-
     for (let i = 0; i < arr.length ; i++) {
         if (i != arr.length -1){
             out = out + arr[i] + " ";
